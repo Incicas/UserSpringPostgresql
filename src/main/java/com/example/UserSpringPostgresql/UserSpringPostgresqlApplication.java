7476e140c2f0923f371ccec5d36f13dcc68fb882
Beni <incicas.beniamin@gmail.com>
2022-01-26 12:18:11 +0200

    Start Project

---
+++ src/main/java/com/example/UserSpringPostgresql/UserSpringPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.UserSpringPostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSpringPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSpringPostgresqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UserSpringPostgresql/UserSpringPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UserSpringPostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSpringPostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
